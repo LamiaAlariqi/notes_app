9bd5ba9eca0ea3be299a9646b501962213d89a92
lamia <lamiaalariqi123@gmail.com>
2024-05-02 16:51:05 +0300

    create type adapter for note

--- lib/models/notemodel.dart
+++
@@ -1,9 +0,0 @@
-class Notemodel{
-final String title;
-final String subtitle;
-final String date;
-final int color;
-
-  Notemodel({required this.title, required this.subtitle, required this.date, required this.color});
-
-}
