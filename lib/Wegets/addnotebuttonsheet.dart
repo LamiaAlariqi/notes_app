5c06dec48b443657942ce7063273e870ce31ccb3
lamia <lamiaalariqi123@gmail.com>
2024-05-03 16:15:42 +0300

    take input from the user

--- lib/Wegets/addnotebuttonsheet.dart
+++ lib/Wegets/addnotebuttonsheet.dart
@@ -1,4 +1,5 @@
  import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:notes_app/Wegets/customButton.dart';
 import 'package:notes_app/Wegets/customTextfield.dart';
 
@@ -7,20 +8,60 @@ class addnotebuttonsheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16),
       child: SingleChildScrollView(
-        child: Column(
-          children:  [
-            SizedBox(height: 32,),
-            customTextfield(hint: 'Title',),
-            SizedBox(height: 16,),
-            customTextfield(hint: 'Content',maxline: 5,),
-              SizedBox(height: 32,),
-            custombutton(),
-            SizedBox(height: 16,),
-          ],
-        ),
+        child: AddNoteForm(),
+      ),
+    );
+  }
+}
+
+class AddNoteForm extends StatefulWidget {
+  const AddNoteForm({
+    super.key,
+  });
+
+  @override
+  State<AddNoteForm> createState() => _AddNoteFormState();
+}
+
+class _AddNoteFormState extends State<AddNoteForm> {
+  @override
+  final GlobalKey <FormState>formkey=GlobalKey();
+  AutovalidateMode autovalidateMode=AutovalidateMode.disabled;
+  String ? title,subtitle;
+  Widget build(BuildContext context) {
+    return Form(
+      key: formkey,
+      autovalidateMode: autovalidateMode,
+      child: Column(
+        children: [
+          const SizedBox(height: 32,),
+          customTextfield(hint: 'Title',onSaved:(value){
+             title=value;
+          },),
+          const SizedBox(height: 16,),
+          customTextfield(hint: 'Content',maxline: 5, onSaved: (value){
+            subtitle=value;
+          },),
+           const SizedBox(height: 32,),
+          custombutton( 
+            onTap: (){
+              if(formkey.currentState!.validate()){
+                formkey.currentState!.save();
+              }
+              else{
+                autovalidateMode=AutovalidateMode.always;
+                setState(() {
+                  
+                });
+              }
+            },
+
+          ),
+          const SizedBox(height: 16,),
+        ],
       ),
     );
   }
